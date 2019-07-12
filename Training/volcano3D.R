20bee76c26bf4c75525209717a7baedf4bad1e2a
Wanjun Gu <wag001@UCSD.EDU>
2019-07-11 18:39:18 -0700

    Ian Auto Commit

---
+++ Training/volcano3D.R
@@ -0,0 +1,18 @@
+car = as.matrix(cars)
+
+car = cbind(car, car,car, car,car, car,car, car,car, car,car, car)
+
+y <- 2 * car     # Exaggerate the relief
+
+x <- 10 * (1:nrow(y))   # 10 meter spacing (S to N)
+z <- 10 * (1:ncol(y))   # 10 meter spacing (E to W)
+
+ylim <- range(y)
+ylen <- ylim[2] - ylim[1] + 1
+
+colorlut <- terrain.colors(ylen) # height color lookup table
+
+col <- colorlut[ y - ylim[1] + 1 ] # assign colors to heights for each point
+
+rgl.open()
+rgl.surface(x, z, y, color = col, back = "lines")
